e540d41a849c1ba5dbf82dccb17f89fa0e2c5c73
alexandra9402kr <alexandra94.02kr@mail.ru>
2021-05-06 14:20:56 +0300

    галерея

---
+++ gallery.js
@@ -0,0 +1,9 @@
+let forward = () => {
+    let images = document.getElementById ('images');
+    images.src = "./images/sterling-davis-4iXagiKXn3Y-unsplash.jpg";
+}
+
+let back = () => {
+    let images = document.getElementById ('images');
+    images.src = "./images/tianshu-liu-aqZ3UAjs_M4-unsplash.jpg"
+}
